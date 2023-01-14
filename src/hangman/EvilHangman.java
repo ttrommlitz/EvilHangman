d26ba13251f386636fe708d07faf6717ad365f93
Tyler Trommlitz <ttrommlitz6@gmail.com>
2023-01-13 23:04:34 -0700

    write main entrypoint

--- src/hangman/EvilHangman.java
+++ src/hangman/EvilHangman.java
@@ -2,38 +2,89 @@ package hangman;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class EvilHangman {
 
     public static void main(String[] args) {
         var eh = new EvilHangman();
         EvilHangmanGame game = new EvilHangmanGame();
+        game.setNumGuesses(Integer.parseInt(args[2]));
         try {
             game.startGame(new File(args[0]), Integer.parseInt(args[1]));
+            Set<String> res = new HashSet<>();
+            res.add("This should never be displayed");
+            while (game.getNumGuesses() > 0) {
+                res = eh.playRound(game);
+                if (!game.getGuessedPortion().contains("" + '-')) {
+                    System.out.println("You win! The correct word was: " + res.iterator().next());
+                    break;
+                }
+            }
+            if (game.getNumGuesses() == 0) {
+                System.out.println("You lose! The correct word was: " + res.iterator().next());
+            }
         } catch (EmptyDictionaryException ex) {
             ex.printMessage();
         } catch (IOException ex) {
-            System.out.println(ex);
-        }
-
-        for (int i = 0; i < Integer.parseInt(args[1]); i++) {
-            eh.playRound(game);
+            System.out.println("IOException: Do better next time lol");
         }
     }
 
     //helpers
-    private void playRound(EvilHangmanGame game) {
-        System.out.println("You have " + game.getNumGuesses() + " guesses left\n");
+    private Set<String> playRound(EvilHangmanGame game) {
+        System.out.println("You have " + game.getNumGuesses() + " guesses left");
+        System.out.println("Used letters: [" + getAlreadyGuessed(game) + "]");
+        System.out.println("Word: " + game.getGuessedPortion());
+        System.out.println("Enter guess: ");
+
+        var scanner = new Scanner(System.in);
+        boolean badInput = false;
+        Set<String> result = null;
+        do {
+            String input = scanner.nextLine();
+            if (input.length() != 1 || !Character.isLetter(input.charAt(0))) {
+                badInput = true;
+                System.out.println("Bad Input! Try again: ");
+                continue;
+            }
+            try {
+                result = game.makeGuess(input.charAt(0));
+                int numCorrectLetters = 0;
+                for (int i = 0; i < result.iterator().next().length(); i++) {
+                    if (result.iterator().next().charAt(i) == input.charAt(0)) {
+                        numCorrectLetters++;
+                    }
+                }
+                if (numCorrectLetters > 0) {
+                    System.out.println("Yes, there is " + numCorrectLetters + " " + input.charAt(0) + "\n");
+                    badInput = false;
+                } else {
+                    System.out.println("Sorry, there are no " + input.charAt(0) + "\n");
+                    badInput = false;
+                }
+
+            } catch (GuessAlreadyMadeException ex) {
+                badInput = true;
+                ex.printMessage();
+            }
+        } while (badInput);
+        return result;
+
+    }
+
+    private String getAlreadyGuessed(EvilHangmanGame game) {
         StringBuilder sb = new StringBuilder();
         for (char element : game.getGuessedLetters()) {
             sb.append(element);
             sb.append(", ");
         }
-        sb.delete(sb.length() - 2, sb.length());
-        System.out.println("Used letters: [" + sb + "]\n");
-        System.out.println("Word: " + game.getGuessedLetters() + "\n");
-        System.out.println("Enter guess: ");
-
+        if (game.getGuessedLetters().size() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+        return sb.toString();
     }
 
 }
--- src/hangman/EvilHangmanGame.java
+++ src/hangman/EvilHangmanGame.java
@@ -6,22 +6,24 @@ import java.util.*;
 
 public class EvilHangmanGame implements IEvilHangmanGame {
     private int numGuesses;
+    private int wordLength;
     private HashSet<String> wordSet;
     private HashMap<String, HashSet<String>> partition;
-    private HashSet<Character> guesses;
-    private TreeSet<Character> guessedPortion;
+    private TreeSet<Character> wrongGuesses;
+    private String guessedPortion;
 
     public EvilHangmanGame() {
-        guesses = new HashSet<>();
+        wrongGuesses = new TreeSet<>();
     }
 
     @Override
     public void startGame(File dictionary, int wordLength) throws IOException, EmptyDictionaryException {
-        numGuesses = wordLength;
-        guessedPortion = new TreeSet<>();
+        this.wordLength = wordLength;
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < wordLength; i++) {
-            guessedPortion.add('_');
+            sb.append("-");
         }
+        guessedPortion = sb.toString();
         var scanner = new Scanner(dictionary);
         wordSet = new HashSet<>();
         while (scanner.hasNext()) {
@@ -36,7 +38,7 @@ public class EvilHangmanGame implements IEvilHangmanGame {
 
     @Override
     public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
-        if (!guesses.add(Character.toLowerCase(guess))) { throw new GuessAlreadyMadeException(); }
+        if (!wrongGuesses.add(Character.toLowerCase(guess))) { throw new GuessAlreadyMadeException(); }
         partition = new HashMap<>();
         for (String element : wordSet) {
             var key = getSubsetKey(element, guess);
@@ -48,17 +50,25 @@ public class EvilHangmanGame implements IEvilHangmanGame {
                 partition.put(key, set);
             }
         }
-        wordSet = Tiebreaker(partition);
+        wordSet = Tiebreaker(partition, guess);
         return wordSet;
     }
 
     @Override
     public SortedSet<Character> getGuessedLetters() {
-        return guessedPortion;
+        return wrongGuesses;
     }
 
     public int getNumGuesses() {
         return numGuesses;
+    }
+
+    public void setNumGuesses(int guesses) {
+        numGuesses = guesses;
+    }
+
+    public String getGuessedPortion() {
+        return guessedPortion;
     }
 
     //helpers
@@ -68,21 +78,84 @@ public class EvilHangmanGame implements IEvilHangmanGame {
             if (word.charAt(i) == guessedLetter) {
                 sb.append(guessedLetter);
             } else {
-                sb.append('_');
+                sb.append('-');
             }
         }
         return sb.toString();
     }
 
-    private HashSet<String> Tiebreaker(HashMap<String, HashSet<String>> partition) {
+    private HashSet<String> Tiebreaker(HashMap<String, HashSet<String>> partition, char guess) {
         int maxSize = 0;
-        var maxSet = new HashSet<String>();
+        var maxSets = new HashMap<String, HashSet<String>>();
         for (Map.Entry<String, HashSet<String>> currEntry : partition.entrySet()) {
-            if (currEntry.getValue().size() > maxSize) {
-                maxSet = currEntry.getValue();
+            if (currEntry.getValue().size() == maxSize) {
+                maxSets.put(currEntry.getKey(), currEntry.getValue());
+            } else if (currEntry.getValue().size() > maxSize) {
                 maxSize = currEntry.getValue().size();
+                maxSets = new HashMap<>();
+                maxSets.put(currEntry.getKey(), currEntry.getValue());
             }
         }
-        return maxSet;
+        if (maxSets.size() == 1) { return theReturn(maxSets.entrySet().iterator().next()); }
+
+        var newMaxSets = new HashMap<String, HashSet<String>>();
+        int letterCount = wordLength;
+        for (var currEntry : maxSets.entrySet()) {
+            if (!currEntry.getKey().contains("" + guess)) {
+                return theReturn(currEntry);
+            }
+            int currCount = 0;
+            for (int j = 0; j < currEntry.getKey().length(); j++) {
+                if (currEntry.getKey().charAt(j) == guess) { currCount++; }
+            }
+            if (currCount == letterCount) { newMaxSets.put(currEntry.getKey(), currEntry.getValue()); }
+            else if (currCount < letterCount) {
+                letterCount = currCount;
+                newMaxSets = new HashMap<>();
+                newMaxSets.put(currEntry.getKey(), currEntry.getValue());
+            }
+        }
+        if (newMaxSets.size() == 1) { return theReturn(newMaxSets.entrySet().iterator().next()); }
+
+        int i = 0;
+        while (newMaxSets.size() != 1) {
+            maxSets = new HashMap<>();
+            for (var currEntry : newMaxSets.entrySet()) {
+                if (currEntry.getKey().charAt(currEntry.getKey().length() - 1 - i) == guess) {
+                    maxSets.put(currEntry.getKey(), currEntry.getValue());
+                }
+            }
+            i++;
+            if (maxSets.size() != 0) {
+                newMaxSets = maxSets;
+            }
+        }
+        for (var currEntry : newMaxSets.entrySet()) {
+            return theReturn(currEntry);
+        }
+        return null;
+    }
+
+    private HashSet<String> theReturn(Map.Entry<String, HashSet<String>> lastEntry) {
+        boolean correctGuess = false;
+        for (int i = 0; i < lastEntry.getKey().length(); i++) {
+            if (lastEntry.getKey().charAt(i) != '-') {
+                correctGuess = true;
+                break;
+            }
+        }
+        if (!correctGuess) {
+            numGuesses--;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < guessedPortion.length(); i++) {
+            if (guessedPortion.charAt(i) == '-' && lastEntry.getKey().charAt(i) != '-') {
+                sb.append(lastEntry.getKey().charAt(i));
+            } else {
+                sb.append(guessedPortion.charAt(i));
+            }
+        }
+        guessedPortion = sb.toString();
+        return lastEntry.getValue();
     }
 }
